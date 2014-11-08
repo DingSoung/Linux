d72d795804cac530eba012c3b7bb7aa30b6df560
DingSoung <DingSoung@gmail.com>
2014-11-08 06:26:33 +0000

    add auto run shel for beagle

--- beagleBoneBlack/mountTF.sh
+++ beagleBoneBlack/mountTF.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Setting mount TF Card to ~/TF"
 umount /dev/mmcblk1p1
-mount /dev/mmcblk1p1 ~/TF
+mount /dev/mmcblk1p1 ~/tf_card
--- common/systemTime.sh
+++ common/systemTime.sh
@@ -3,6 +3,5 @@ echo "current system time"
 date
 echo "Setting the time"
 ntpdate time.nist.gov
-tzselect China
 echo "New time"
 date 
