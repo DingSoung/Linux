2a6d132b6a8eb1f264e8c65c8e8cf470d730deea
root <root@default.hostname>
2016-12-30 04:46:19 -0500

    rename

--- centOS/shadowsocks.sh
+++ centOS/shadowsocks.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 clear 
 
-echo "安装shadowsocks环境..."
 yum install python-setuptools && easy_install pip
 pip install shadowsocks
 
-echo "代理端口1324，密码aaaaaa"
 ssserver -p 1324 -d stop
 ssserver -p 1324 -k aaaaaa -d start
 
-echo "完成"
