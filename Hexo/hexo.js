5dfa8cdaed4b2d851663f741f0376af2315ff284
root <root@default.hostname>
2017-08-06 07:01:43 -0400

    ddd

---
+++ Hexo/hexo.js
@@ -0,0 +1,5 @@
+#!/bin/sh
+clear 
+
+npm install -g hexo-cli
+
---
+++ Hexo/nvm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+clear 
+
+wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+echo "exit shell and re-enter, run hexo"
