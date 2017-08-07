2637ada0529bc963426da4e9933725e930c3d898
root <root@default.hostname>
2017-08-07 09:29:32 -0400

    update mySQL config

--- LNMP/install.sh
+++ LNMP/install.sh
@@ -3,15 +3,21 @@ clear
 
 yum install epel-release
 
+# install Nginx
 yum install nginx
 
-yum install mysql mysql-server
+# download, install and verfy mySQL repo
+wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+yum localinstall mysql57-community-release-el7-7.noarch.rpm 
+yum repolist enabled | grep "mysql.*-community.*"
+rm -rf mysql57-community-release-el7-7.noarch.rpm
 
+# install mySQL and check version
+yum install mysql-community-server
+mysql --version
+
+#install PHP
 yum install php
 yum install php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc
-
-chkconfig nginx on
- 
-chmod -R o+r ~/
 
 sh ./start.sh
--- LNMP/start.sh
+++ LNMP/start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 clear
 
-/etc/init.d/mysqld start
+# start mySQL and check status
+service mysqld start
+service mysqld status
 
 # mount nginx config and html
 mount -B ./conf.d /etc/nginx/conf.d/
 mount -B ~/html /usr/share/nginx/html
+
+# add permison for nginx
+chmod -R o+r ~/
+
+# set nginx auto start
+chkconfig nginx on
 
 # restart nginx
 fuser -k 80/tcp
