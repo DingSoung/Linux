3535df5b96d6e34849bd6f897d7c227065b4e6d9
root <root@default.hostname>
2017-08-07 09:04:14 -0400

    use nginx 80, but mount html to home

--- LNMP/start.sh
+++ LNMP/start.sh
@@ -3,8 +3,11 @@ clear
 
 /etc/init.d/mysqld start
 
+# mount nginx config and html
 mount -B ./conf.d /etc/nginx/conf.d/
+mount -B ~/html /usr/share/nginx/html
 
+# restart nginx
 fuser -k 80/tcp
 nginx
 nginx -s reload
